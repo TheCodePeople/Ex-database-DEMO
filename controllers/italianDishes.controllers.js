6b3bd995e2313b8765b059fb5a70b28a02364575
ZehraaMA <zahraa@thecodepeople.io>
2024-04-29 13:35:16 +0300

    use mongoose queries to manipulate the data

--- controllers/italianDishes.controllers.js
+++ controllers/italianDishes.controllers.js
@@ -1,68 +1,62 @@
-const uuid4 = require("uuid");
-
-const italianDishes = require("../italianDishes");
+const Dish = require("../models/Dish");
 
 exports.getAllItalianDishes = async (req, res) => {
   try {
-    res.status(200).json(italianDishes); // Return the Italian dishes as JSON
+    const allDishes = await Dish.find();
+    res.status(200).json({ allDishes });
   } catch (error) {
-    return res.status(500).json({ message: "Internal server error" });
+    return res
+      .status(500)
+      .json({ message: error.message || "Internal server error" });
   }
 };
 
 exports.getOneDish = async (req, res) => {
   try {
     const { id } = req.params;
-    const foundDish = italianDishes.find((dish) => dish.id === Number(id));
+    const foundDish = await Dish.findById(id);
     res.status(200).json(foundDish); // Return the Italian dishes as JSON
   } catch (error) {
-    return res.status(500).json({ message: "Internal server error" });
+    return res
+      .status(500)
+      .json({ message: error.message || "Internal server error" });
   }
 };
 
 exports.createDish = async (req, res) => {
   try {
-    const newDishData = { ...req.body, id: uuid4() };
-
-    // Add the new dish to your menu (e.g., an array of dishes)
-    italianDishes.push(newDishData);
-
-    // Send a response with the newly created dish
-    res.status(201).json(newDish);
+    const newDish = await Dish.create(req.body);
+    res.status(201).json({ newDish });
   } catch (error) {
-    return res.status(500).json({ message: "Internal server error" });
+    return res
+      .status(500)
+      .json({ message: error.message || "Internal server error" });
   }
 };
 
 exports.deleteDish = async (req, res) => {
   try {
-    const dishId = req.params.id;
-    const dishIndex = italianDishes.findIndex((dish) => dish.id === +dishId);
-    if (dishIndex !== -1) {
-      italianDishes.splice(dishIndex, 1);
-      res.status(204).end(); // Respond with a 204 No Content status code
-    } else {
-      res.status(404).json({ error: "Dish not found" }); // Respond with a 404 Not Found status code and an error message
-    }
+    const { id } = req.params;
+    await Dish.findByIdAndDelete(id);
+    res.status(204).json({ message: "Deleted successfully" });
   } catch (error) {
-    return res.status(500).json({ message: "Internal server error" });
+    return res
+      .status(500)
+      .json({ message: error.message || "Internal server error" });
   }
 };
 
 exports.updateDish = async (req, res) => {
   try {
-    const dishId = req.params.id;
-    const updatedDishData = req.body;
-    // Implement the code to update the dish in the array
-    const index = italianDishes.findIndex((dish) => dish.id === Number(dishId));
-    if (index !== -1) {
-      // Update the dish with the new data
-      italianDishes[index] = { ...italianDishes[index], ...updatedDishData };
-      res.status(201).json(italianDishes[index]); // Respond with the updated dish
-    } else {
-      res.status(404).json({ error: "Dish not found" }); // Respond with a 404 Not Found status code and an error message
-    }
+    const { id } = req.params;
+    const updatedData = req.body;
+    const updatedDish = await Dish.findByIdAndUpdate(id, updatedData, {
+      new: true,
+    });
+    res.status(201).json({ updatedDish });
   } catch (error) {
-    return res.status(500).json({ message: "Internal server error" });
+    return res
+      .status(500)
+      .json({ message: error.message || "Internal server error" });
   }
 };
--- routes/italianDishes.routes.js
+++ routes/italianDishes.routes.js
@@ -7,21 +7,19 @@ const {
   getOneDish,
   deleteDish,
   updateDish,
+  createDish,
 } = require("../controllers/italianDishes.controllers");
 
-// Define a route for all /dishes
 router.get("/", getAllItalianDishes);
 
-// Define a route for one /dishes
 router.get("/:id", getOneDish);
 
 // Create a middleware for creating a dish
-router.post("/");
+router.post("/", createDish);
 
-// Create a middleware for deleting a dish
+//
 router.delete("/:id", deleteDish);
 
-// Create a middleware for updating a dish
 router.put("/:id", updateDish);
 
 module.exports = router;
